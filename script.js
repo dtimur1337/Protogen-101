abe5b81abb260e61e25f7f842575410276ae895b
Timur <timur.dzhambinov@slalom.com>
2026-05-23 13:33:32 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// Smooth scroll behavior for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+            target.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    });
+});
+
+// Add scroll effect to navigation
+let lastScrollTop = 0;
+window.addEventListener('scroll', () => {
+    lastScrollTop = window.scrollY;
+});
